1c60675504ce1a2ddb8907a7b345381074b6392c
brunonic17 <bscistri@gmail.com>
2023-08-06 20:12:51 -0300

    piramide terminada

--- piramide.js
+++ piramide.js
@@ -1,7 +1,14 @@
-let s
-let i
-let p
+let n=5
+let p= 1
 
-for(i=0;i<=4;i++){
-    console.log(i)
-}
+for(let i=0;i<=n;i++){
+    for(let j=0;j<n-i; j++){
+        
+        p +=" "
+      }
+       for(let k=0;k<2*i-1; k++){
+        p +=1
+    }
+    p +="\n"
+  }
+  console.log(p)
