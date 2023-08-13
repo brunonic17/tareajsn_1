0fee466571104213331e101558a70427525eedf8
MauroScistri <scistrimauro123@gmail.com>
2023-08-13 12:46:37 -0300

    logrando primer paso de la facruracion

--- facturacion.js
+++ facturacion.js
@@ -1,19 +1,41 @@
-let n=5
-let p= ""
+let U="Ingresar Usuario"
+let M=""
+let C="Ingrese el monto a pagar"
+var CP=prompt(C)
+var IM ="ingrese un monto númerico por favor"
+var IMP=prompt(IM)
+var IU=prompt(U)
+var EU="error, porfavor coloque su usuario"
+var EUP=prompt(EU)
+var P="¿Como desea pagar?"
+var CP=prompt(P)
+var OP="Opciones de pago";"1-efectivo";"2-Tarjeta";"3-Mercadopago";"4-Obra social"
+var OPP=prompt(OP)
+let MP=""
 
 
-for(let i=0;i<=n;i++){
-    for(let j=0;j<n-i; j++){
-        
-        p +=" "
-      }
-       for(let k=0;k<2*i-1; k++){
-        p +="*"
-    }
-    p+="\n"
-    
-  } 
-
-console.log(p)
  
+if(IU=String){
+    CP
+}
+else{
+    EUP  
+}
+console.log(IU)
+// if(CP=Number){
+//     switch(OPP){
+//     case 1:
+//      break
+//      case 2:
+//         break
+//     case 3:
+//         break
+//     case 4:
+//     }
+// }
+// else{
+//    IMP
+// }
+
+
  
